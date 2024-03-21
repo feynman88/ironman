efd2b3320a12aed4e1ae83942280d949841f8d56
Omar <44785357+feynman88@users.noreply.github.com>
2024-03-21 23:14:03 +0200

    created a node app

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hello index js')
+
+
+const express = require('express');
+const app = express();
+
+app.get("/" , (req , res)=>{
+
+    res.send ('hello world')
+
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
